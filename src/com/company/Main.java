83df62ad69bea969b5fcf250e0ef2602570c726a
Elmirbek2005 <elmirbek2005.kg@gmail.com>
2022-02-11 17:44:26 +0600

    fgfgh

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,26 @@
 package com.company;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
         double[] numbers = {-5.7, 3.5, 5.6, -7.5, 3.9, -9.9, 9.9, 5.5, -5.6, 2.4, -2.6, -8.0, 7.7, -5.1, 7.9};
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] < 0) {
-                continue;
-            }
-            System.out.println(numbers[i]);
-        }
-    }
+      double lol=0;
+      int haker =0;
+      boolean hook=false;
+      for (double nabers:numbers){
+          if (nabers<0){
+              hook = true;
+          }else if (hook){
+              haker++;
+              lol +=nabers;
+          }
+      }
+        System.out.println(lol / haker);
+      }
+
 
     public static void game () {
         int[] arr = {-4, -2, 2, 3, 6, 8};
